2f20c28c660e399080832e3b25592f3ec037efcc
tamari <tamarigray97@gmail.com>
2019-08-10 21:53:40 +1200

    installed frommet

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
++ import { Grommet } from 'grommet';
+
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+ 
+    </div>
+  );
+}
+
+export default App;
