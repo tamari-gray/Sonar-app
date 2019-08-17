e7675d6e0ca219850255785cb25ae71b6dfc3547
tamari <tamarigray97@gmail.com>
2019-08-17 21:50:46 +1200

    use redux to check auth

--- src/components/Auth/SignUp.jsx
+++ src/components/Auth/SignUp.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Link } from "react-router-dom"
+import { Link, Redirect } from "react-router-dom"
 import { Box, FormField, TextInput, Form, Button } from "grommet"
+import { createAccount } from '../../actions/user';
 
 class SignUp extends Component {
 
   state = {
-    email: '',
-    password: '',
     firstName: '',
-    lastName: ''
+    lastName: '',
+    email: '',
+    password: ''
   }
 
   handleChange = (e) => {
@@ -20,80 +21,81 @@ class SignUp extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault()
+    this.props.dispatch(createAccount(this.state))
   }
 
   render() {
     const { email, password, firstName, lastName } = this.state
-    return (
-      <Box
-        align="center"
-        pad="large"
-      >
-        <h2>
-          Welcome to sonar
-        </h2>
+    if (this.props.authed !== null) {
+      return <Redirect to='/profile' />
+    } else {
+      return (
         <Box
-          pad="medium"
-          border={{ color: 'brand', size: 'large' }}
-          elevation="medium"
-          round="large"
+          align="center"
+          pad="large"
         >
-          <Form
-            style={{margin: '1.5em 1.5em 0 1.5em '}}
-            onSubmit={this.handleSubmit}
+          <h2>
+            Welcome to sonar
+        </h2>
+          <Box
+            pad="medium"
+            border={{ color: 'brand', size: 'large' }}
+            elevation="medium"
+            round="large"
           >
-            <FormField required={true}>
-              <TextInput
-                placeholder="first name"
-                name="firstName"
-                value={firstName}
-                onChange={this.handleChange}
-              />
-            </FormField>
-            <FormField >
-              <TextInput
-                placeholder="last name"
-                name="lastName"
-                onChange={this.handleChange}
-                value={lastName}
-              />
-            </FormField>
-            <FormField >
-              <TextInput
-                placeholder="email"
-                name="email"
-                onChange={this.handleChange}
-                value={email}
-              />
-            </FormField>
-            <FormField >
-              <TextInput
-                placeholder="password"
-                type="password"
-                name="password"
-                onChange={this.handleChange}
-                value={password}
-              />
-            </FormField>
-            <Box direction="row" justify="center" margin={{ top: "large" }}>
-              <Button type="submit" primary label="Sign up" />
+            <Form
+              style={{ margin: '1.5em 1.5em 0 1.5em ' }}
+              onSubmit={this.handleSubmit}
+            >
+              <FormField required={true}>
+                <TextInput
+                  placeholder="first name"
+                  name="firstName"
+                  value={firstName}
+                  onChange={this.handleChange}
+                />
+              </FormField>
+              <FormField >
+                <TextInput
+                  placeholder="last name"
+                  name="lastName"
+                  onChange={this.handleChange}
+                  value={lastName}
+                />
+              </FormField>
+              <FormField >
+                <TextInput
+                  placeholder="email"
+                  name="email"
+                  onChange={this.handleChange}
+                  value={email}
+                />
+              </FormField>
+              <FormField >
+                <TextInput
+                  placeholder="password"
+                  type="password"
+                  name="password"
+                  onChange={this.handleChange}
+                  value={password}
+                />
+              </FormField>
+              <Box direction="row" justify="center" margin={{ top: "large" }}>
+                <Button type="submit" primary label="Sign up" />
+              </Box>
+            </Form>
+            <Box direction="row" justify="center">
+              <p>Already have an account? <Link to="/login">Login here</Link></p>
             </Box>
-          </Form>
-          <Box direction="row" justify="center">
-            <p>Already have an account? <Link to="/login">Login here</Link></p>
           </Box>
         </Box>
-      </Box>
-    )
+      )
+    }
   }
 }
 
-const mapStateToProps = (state) => ({
-
+const mapStateToProps = ({ user }) => ({
+  authed: user
 })
 
-const mapDispatchToProps = {
-
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(SignUp)
+export default connect(mapStateToProps)(SignUp)
