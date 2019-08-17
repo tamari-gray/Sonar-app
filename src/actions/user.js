c879d9cc8ae926f0f9127db5cfd42f786c32afd4
tamari <tamarigray97@gmail.com>
2019-08-17 21:49:37 +1200

    started getUser action and reducer

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,4 +1,45 @@
-export const createUser = (payload) => ({
-  type: "USER",
-  payload
-})
+import { auth, db } from "../firebase";
+
+export const GET_USER = 'GET_USER'
+export const GET_USER_INFO = 'GET_USER_INFO'
+
+// export function getUser () {
+//   return dispatch => {
+// auth.onAuthStateChanged(user => {
+//   console.log('logged in')
+//   dispatch({
+//     type: GET_USER,
+//     payload: user
+//   })
+// })
+//   }
+// }
+
+export function getUser(userId) {
+  return dispatch => db.ref('users/' + userId).on('value', (snapshot) => {
+    const user = snapshot.val()
+    console.log("got user info action",user)
+    dispatch({
+      type: GET_USER_INFO,
+      payload: snapshot.val()
+    })
+  })
+
+}
+
+export function createAccount(data) {
+  const { firstName, lastName, email, password } = data
+
+  return dispatch => auth.createUserWithEmailAndPassword(email, password)
+    .then(({ user }) => {
+      if (user !== null) {
+        db.ref('users').child(user.uid).set({ firstName, lastName })
+      }
+      return user
+    }).then((user) => {
+      dispatch({
+        type: GET_USER,
+        payload: user.uid
+      })
+    })
+}
