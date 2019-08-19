e16454f0cbfc4d6b5c057df49f906fab924f4244
tamari <tamarigray97@gmail.com>
2019-08-19 22:07:52 +1200

    cleaned up user actions

--- src/actions/user.js
+++ src/actions/user.js
@@ -3,29 +3,13 @@ import { auth, db } from "../firebase";
 export const GET_USER = 'GET_USER'
 export const GET_USER_INFO = 'GET_USER_INFO'
 
-// export function getUser () {
-//   return dispatch => {
-// auth.onAuthStateChanged(user => {
-//   console.log('logged in')
-//   dispatch({
-//     type: GET_USER,
-//     payload: user
-//   })
-// })
-//   }
-// }
-
 export function getUser(userId) {
   return dispatch => db.ref('/users/' + userId).once('value', (snapshot) => {
-    console.log('getting user working')
-    const user = snapshot.val()
-    console.log("got user info action", user)
     dispatch({
       type: GET_USER_INFO,
       payload: snapshot.val()
     })
   })
-
 }
 
 export function createAccount(data) {
@@ -42,22 +26,15 @@ export function createAccount(data) {
         type: GET_USER,
         payload: user.uid
       })
-      return user
     }).catch((error) => console.log(error))
 }
 
 export function loginUser(data) {
-  // return dispatch => console.log('logging in ', data)
   return dispatch => auth.signInWithEmailAndPassword(data.email, data.password)
     .then(({ user }) => {
       dispatch({
         type: GET_USER,
         payload: user.uid
       })
-      return user
     }).catch((error) => console.log(error))
-}
-
-export function wat(data) {
-  return dispatch => console.log('js wat', data)
 }
