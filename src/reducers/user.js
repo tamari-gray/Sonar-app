661db526744229c3e1e988c83157a4a02e1ef8d5
tamari <tamarigray97@gmail.com>
2019-08-21 20:15:41 +1200

    modified reducer. getUser wont overide getUserInfo

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -5,7 +5,7 @@ const initialState = null
 export default (state = initialState, { type, payload }) => {
   switch (type) {
     case GET_USER:
-      return { UID: payload }
+      return { UID: payload, ...state  }
     case GET_USER_INFO:
       return { ...state, ...payload }
     default:
