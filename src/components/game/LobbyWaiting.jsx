d007d79552292015384e85ea41b39371b55ca3f0
tamari <tamarigray97@gmail.com>
2019-08-23 18:45:41 +1200

    display play button for admin

--- src/components/game/LobbyWaiting.jsx
+++ src/components/game/LobbyWaiting.jsx
@@ -1,47 +1,55 @@
+// TODO: post to firebase 
+
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Box, Button } from 'grommet';
-import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import { getMatch } from '../../actions/match';
 
 class LobbyWaiting extends Component {
   state = {
-    creator: false
-  }
-
-  componentDidMount(){
-    this.props.dispatch(getMatch(this.props.match.matchId))
+    play: false
   }
 
   checkCreator = () => {
-    // if (this.props.user.UID === this.props.match.creatorId) {
-    //   this.setState({
-    //     creator: true
-    //   })
-    // } else {
-    //   this.setState({
-    //     creator: false
-    //   })
-    // }
+    if (this.props.user) {
+      if (this.props.user.UID === this.props.match.creatorId) {
+        return true
+      } else {
+        return false
+      }
+    }
+  }
+
+  handleRedirect = () => {
+    this.setState({
+      play: true
+    })
   }
 
   render() {
-    // this.checkCreator()
-    return (
-      <Box align="center">
-        players:
+    const { match, match: { id } } = this.props
+    if (id) {
+      this.props.dispatch(getMatch(id))
+    }
+    if (this.state.play) {
+      return <Redirect to={`/playing/${id}`} />
+    } else {
+      return (
+        <Box align="center">
+          players:
         {
-          // this.props.match.players.map(player => {
-          //   return <h4>{player[0]}</h4>
-          // })
-          console.log(this.props)
-        }
-        {
-          this.state.creator && <Button as={Link} path={`/game/${this.props.match.matchId}`} label="Play!" primary />
-        }
-      </Box>
+            match.players && Object.values(match.players).map(player => {
+              return <h4 key={player.playerId}> {player.name} </h4>
+            })
+          }
+          {
+            this.checkCreator() && <Button onClick={this.handleRedirect} label="Play!" primary />
+          }
+        </Box>
 
-    )
+      )
+    }
   }
 }
 
@@ -50,4 +58,4 @@ const mapStateToProps = ({ match, user }) => ({
   user
 })
 
-export default connect(mapStateToProps  )(LobbyWaiting)
+export default connect(mapStateToProps)(LobbyWaiting)
