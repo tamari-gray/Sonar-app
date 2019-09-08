978dc2457d5d96d1a50620993e5c7da7d080a6ae
tamari <tamarigray97@gmail.com>
2019-09-08 19:10:54 +1200

    deleted comments

--- src/actions/matches.js
+++ src/actions/matches.js
@@ -1,37 +1,4 @@
-import { db } from "../firebase"
-import { GET_MATCH } from "./match"
+
 
 export const GET_MATCHES = 'GET_MATCHES'
 export const GOT_NO_MATCHES = 'GOT_NO_MATCHES'
-
-// export function getMatches(id) {
-//   return dispatch => db.ref('matches').on('value', (snapshot) => {
-//     let matches = false
-//     if (snapshot.val() !== null) {
-//       matches = Object.values(snapshot.val())
-//     }
-//     if (matches) {
-//       matches.forEach((match) => {
-//         const players = Object.values(match.players)
-//         let playerLocations = []
-//         if (match.playerLocations) {
-//           playerLocations = Object.values(match.playerLocations)
-//         }
-//         const alreadyJoined = players.find(player => player.playerId === id)
-//         match.players = players
-//         match.playerLocations = playerLocations
-//         if (alreadyJoined) {
-//           dispatch({
-//             type: GET_MATCH,
-//             payload: match
-//           })
-//         } else {
-//           dispatch({
-//             type: GET_MATCHES,
-//             payload: snapshot.val()
-//           })
-//         }
-//       })
-//     }
-//   })
-// }
