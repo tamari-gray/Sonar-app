dee56ede6aab006f203729d72ca459e69b4a7b2c
tamari <tamarigray97@gmail.com>
2019-09-08 17:50:34 +1200

    deleted rewritten functions

--- src/actions/match.js
+++ src/actions/match.js
@@ -6,67 +6,67 @@ export const JOINED_MATCH = 'JOINED_MATCH'
 export const GET_MATCH = 'GET_MATCH'
 export const PLAY_GAME = 'PLAY_GAME'
 
-export function createMatch(creatorName, creatorId, matchName, password) {
-  return dispatch => {
-    const newMatch = db.ref('matches').push()
-    newMatch.set({
-      creatorName,
-      creatorId,
-      matchName,
-      password,
-      matchId: newMatch.key,
-      inGame: false
-    })
-      .then(() => {
-        const newPlayer = db.ref('/matches/' + newMatch.key).child('players').push()
-        newPlayer.set({
-          playerId: creatorId,
-          name: creatorName
-        })
-      })
-      .then(() => {
-        db.ref('matches').once('value', snapshot => {
-          dispatch({
-            type: CREATED_MATCH,
-            payload: newMatch.key
-          })
-        })
-      })
-  }
-}
+// export function createMatch(creatorName, creatorId, matchName, password) {
+//   return dispatch => {
+//     const newMatch = db.ref('matches').push()
+//     newMatch.set({
+//       creatorName,
+//       creatorId,
+//       matchName,
+//       password,
+//       matchId: newMatch.key,
+//       inGame: false
+//     })
+//       .then(() => {
+//         const newPlayer = db.ref('/matches/' + newMatch.key).child('players').push()
+//         newPlayer.set({
+//           playerId: creatorId,
+//           name: creatorName
+//         })
+//       })
+//       .then(() => {
+//         db.ref('matches').once('value', snapshot => {
+//           dispatch({
+//             type: CREATED_MATCH,
+//             payload: newMatch.key
+//           })
+//         })
+//       })
+//   }
+// }
 
-export function joinMatch(matchId, playerId, playerName) {
-  return dispatch => {
-    const newPlayer = db.ref('/matches/' + matchId).child('players').push()
-    newPlayer.set({
-      playerId,
-      name: playerName
-    })
-      .then(() => {
-        dispatch({
-          type: JOINED_MATCH,
-          payload: matchId
-        })
-      })
-  }
-}
+// export function joinMatch(matchId, playerId, playerName) {
+//   return dispatch => {
+//     const newPlayer = db.ref('/matches/' + matchId).child('players').push()
+//     newPlayer.set({
+//       playerId,
+//       name: playerName
+//     })
+//       .then(() => {
+//         dispatch({
+//           type: JOINED_MATCH,
+//           payload: matchId
+//         })
+//       })
+//   }
+// }
 
-export function getMatch(matchId) {
-  return dispatch => {
-    db.ref('/matches/' + matchId).on('value', snapshot => {
-      dispatch({
-        type: GET_MATCH,
-        payload: snapshot.val()
-      })
-    })
-    db.ref('/matches/' + matchId).on('child_changed', snapshot => {
-      dispatch({
-        type: GET_MATCH,
-        payload: snapshot.val()
-      })
-    })
-  }
-}
+// export function getMatch(matchId) {
+//   return dispatch => {
+//     db.ref('/matches/' + matchId).on('value', snapshot => {
+//       dispatch({
+//         type: GET_MATCH,
+//         payload: snapshot.val()
+//       })
+//     })
+//     db.ref('/matches/' + matchId).on('child_changed', snapshot => {
+//       dispatch({
+//         type: GET_MATCH,
+//         payload: snapshot.val()
+//       })
+//     })
+//   }
+// }
 
 export function playGame(id) {
   return dispatch => {
