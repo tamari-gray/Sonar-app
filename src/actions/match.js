55dc31acd08e7c10dcb355c517ba76d30c1e9deb
tamari <tamarigray97@gmail.com>
2019-09-09 23:33:06 +1200

    deleted rewritten fns

--- src/actions/match.js
+++ src/actions/match.js
@@ -1,5 +1,4 @@
 import { db } from "../firebase"
-import { GeoFire } from 'geofire'
 
 export const CREATED_MATCH = 'CREATED_MATCH'
 export const JOINED_MATCH = 'JOINED_MATCH'
@@ -11,28 +10,6 @@ export function playGame(id) {
     db.ref('/matches/' + id)
       .update({ inGame: true })
       .catch(err => console.log(err.message))
-  }
-}
-
-// geofire init
-let geo
-
-export function setLocation(matchId, { firstName, userId}, { lat, lng }) {
-  return dispatch => {
-    const ref = db.ref(`matches/${matchId}/playerLocations`)
-    geo = new GeoFire(ref)
-    geo.set({
-      [userId]: [lat, lng],
-    })
-  }
-}
-
-export function getLocations(radius, {lat , lng}){
-  return dispatch => {
-    let geoQuery = geo.query({
-      center: [lat, lng],
-      radius
-    })
   }
 }
 
