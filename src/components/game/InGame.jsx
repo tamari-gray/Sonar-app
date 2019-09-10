c6bad2413c060276bfd8ededf086da0bcdbb58e8
tamari <tamarigray97@gmail.com>
2019-09-10 19:23:55 +1200

    listening to players locations and putting on map

--- src/components/game/InGame.jsx
+++ src/components/game/InGame.jsx
@@ -1,6 +1,8 @@
 /*
 TODO:
-- [] get players location data into map and add icons
+- [] watch all players live location
+- [] check if admin
+- [] admin starts game
 */
 
 import React, { Component } from 'react'
@@ -11,6 +13,8 @@ import 'leaflet/dist/leaflet.css'
 import { db, geo } from '../../firebase'
 
 let map = null
+let playersRefUnsubscribe = null
+let playerMarker = null
 
 class InGame extends Component {
   state = {
@@ -21,7 +25,9 @@ class InGame extends Component {
     position: null,
     zoom: 18,
     admin: false,
-    initialising: false
+    initialising: false,
+    players: null,
+    userLocationFound: false
   }
 
   componentDidMount() {
@@ -33,34 +39,34 @@ class InGame extends Component {
       }).then(() => {
         this.getUserLocation()
       })
-      // if (map === null) this.initMap() // put players on map and inside boundary
       // this.getMatch() // toggle play btn
     }
   }
 
-  getUserLocation = () => {
+  // getUserLocation = () => { // rewrote in mapInit. TODO: delete if rewritten function is successfull
 
-    const success = (pos) => {
-      // add player location to db
-      const point = geo.point(pos.coords.latitude, pos.coords.longitude)
-      db.collection(this.props.matchId).doc(this.props.user.UID).update({
-        position: point.data
-      }).then(() => {
-        this.getPlayers() // gets players from db
-      })
-    }
+  //   const success = (pos) => {
+  //     // add player location to db
+  //     const point = geo.point(pos.coords.latitude, pos.coords.longitude)
+  //     db.collection(this.props.matchId).doc(this.props.user.UID).update({
+  //       position: point.data
+  //     }).then(() => {
+  //       this.getPlayers() // gets players from db
 
-    const error = () => {
-      alert('cant get user location')
-    }
+  //     })
+  //   }
 
-    const watchId = navigator.geolocation.watchPosition(success, error)
+  //   const error = () => {
+  //     alert('cant get user location')
+  //   }
 
-    // use this id to stop watching users position => navigator.geolocation.clearWatch(watchID);
-    this.setState({
-      watchId
-    })
-  }
+  //   const watchId = navigator.geolocation.watchPosition(success, error)
+
+  //   // use this id to stop watching users position => navigator.geolocation.clearWatch(watchID);
+  //   this.setState({
+  //     watchId
+  //   })
+  // }
 
   // realtime observable of players
   // getPlayers = () => { 
@@ -75,10 +81,28 @@ class InGame extends Component {
   // }
 
   getPlayers = () => {
-    db.collection(this.props.matchId).onSnapshot((snap) => {
+    playersRefUnsubscribe = db.collection(this.props.matchId).onSnapshot((querySnapshot) => {
       let players = []
-      snap.forEach(doc => {
+      querySnapshot.forEach(doc => { // watch every players position
         players.push(doc.data())
+        const pos = [doc.data().position.geopoint.latitude, doc.data().position.geopoint.latitude]
+        let playerMarker = L.circle(pos, {
+          color: 'red',
+          fillColor: '#f03',
+          fillOpacity: 0.5,
+          radius: 100
+        }).addTo(map)
+
+        if (doc.ref.id === this.props.user.UID) { // check if player is this user
+          playerMarker = L.circle(pos, { // set player marker to black 
+            color: 'black',
+            fillColor: '#f03',
+            fillOpacity: 0.5,
+            radius: 100
+          }).addTo(map)
+
+          map.setView(pos, 10) // watch this user's position on map
+        }
       })
       this.setState({ players })
       console.log('got players', this.state.players)
@@ -88,15 +112,17 @@ class InGame extends Component {
   getMatch = () => {
     db.collection('matches').doc(this.props.matchId)
       .onSnapshot((doc) => {
-        // check if user is admin
-        if (doc.data().admin.id === this.props.user.UID) {
+        
+        if (doc.data().admin.id === this.props.user.UID) { // check if user is admin
           this.setState({ admin: doc.data().admin })
         }
-        // check if game is in play
-        if (doc.data().initialising) {
+        
+        if (doc.data().initialising) { // check if game is initialising
           this.setState({ initialising: true })
         }
-        this.initMap(this.props.user)
+        if (doc.data().playing) { // check if game is in play
+          this.setState({ playing: true })
+        }
       })
   }
 
@@ -106,19 +132,44 @@ class InGame extends Component {
       maxZoomLevel: 22,
       maxNativeZoom: 22,
       zoomControl: true
-    }).fitWorld()
+    })
+      .fitWorld()
+      .setView([this.state.boundary.lat, this.state.boundary.lng], 19)
 
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       detectRetina: true,
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map)
 
-    map.locate({ setView: true, maxZoom: 19 })
+    const boundary = L.circle([this.state.boundary.lat, this.state.boundary.lng], {
+      color: 'blue',
+      fillColor: '#f03',
+      fillOpacity: 0.5,
+      radius: 200
+    }).addTo(map)
+
+    map.on('locationFound', ((e) => {
+      const point = geo.point(e.latlng.latitude, e.latlng.longitude) // TODO: fix
+      db.collection(this.props.matchId).doc(this.props.user.UID).update({ // update users location in DB
+        position: point.data
+      }).then(() => {
+        this.setState({ // tell component that weve got users location
+          userLocationFound: true
+        })
+      }).then(() => {
+        if (this.state.userLocation) { // only run this.getPlayers once. it has a realtime listener on it
+          this.getPlayers() // gets players from db
+        }
+      })
+    }))
+
   }
 
   playGame = () => {
+    // playersRefUnsubscribe() // remove players ref listener
+
     db.collection('matches').doc(this.props.matchId)
-      .update({ initialising: true })
+      .update({ playing: true })
       .catch(e => console.log(`Error initialising game. ${e}`))
   }
 
@@ -127,16 +178,40 @@ class InGame extends Component {
   }
 
   render() {
-    const { admin, initialising } = this.state
+    const { admin, initialising, players, playing } = this.state
     return (
       <Box align="center" >
         <Box id="map" style={{ height: "480px", width: "100%" }} >
         </Box>
+        <Box width="medium" align="center" >
+          {
+            players && <h3>{players.length} players in game</h3>
+          }
+          {
+            players && players.map(player => {
+              return <Box
+                key={player.name}
+                pad="small"
+                border={{ color: 'primary', size: 'large' }}
+                elevation="small"
+                round="large"
+                width="small"
+                align="center"
+                direction="row-responsive"
+                style={{ marginTop: '1.5em' }}
+              >
+                <h3 style={{ margin: "auto" }}>
+                  {player.name}
+                </h3>
+              </Box>
+            })
+          }
+        </Box>
         {
-          admin && initialising === false && <Button onClick={this.playGame} label="Play!" primary />
+          admin && !initialising && <Button onClick={this.playGame} label="Play!" primary />
         }
         {
-          initialising && <Button primary style={{ padding: '0.8em' }} onClick={this.handleSonar}> send sonar </Button>
+          playing && <Button primary style={{ padding: '0.8em' }} onClick={this.handleSonar}> send sonar </Button>
         }
       </Box>
     )
