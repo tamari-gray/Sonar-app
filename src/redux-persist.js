7d1cfdd18e1ba29fa20a58b94c9ef262ba47b5c6
tamari <tamarigray97@gmail.com>
2019-09-18 20:04:23 +1200

    redux dev tools working on other browsers

--- src/redux-persist.js
+++ src/redux-persist.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
@@ -11,7 +11,7 @@ const persistConfig = {
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(persistedReducer, composeEnhancers(
   applyMiddleware(thunk)
