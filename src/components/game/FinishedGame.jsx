fb457bba9548b66ac45c5f1ea54aa91be68997ba
tamari <tamarigray97@gmail.com>
2019-10-05 16:17:36 +1300

    add link to home page

--- src/components/game/FinishedGame.jsx
+++ src/components/game/FinishedGame.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom';
+import routes from '../../routes';
 
 export class FinishedGame extends Component {
   render() {
     return (
       <div>
         GAME FINISHED
+        <Link to={routes.PROFILE}>
+          go home
+        </Link>
       </div>
     )
   }
