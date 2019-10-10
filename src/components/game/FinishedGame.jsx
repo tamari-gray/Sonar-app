110d5f8913c8e779fce70c61a22d8eca340bea41
tamari-gray <tamari-gray>
2019-10-11 03:00:00 +1300

    started geofire init

--- src/components/game/FinishedGame.jsx
+++ src/components/game/FinishedGame.jsx
@@ -1,17 +1,28 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom';
-import routes from '../../routes';
+import { Link } from 'react-router-dom'
+import routes from '../../routes'
+import { Box } from 'grommet'
+import { db, geo } from '../../firebase'
 
 export class FinishedGame extends Component {
+  state = {
+    winners: []
+  }
+
+  getWinners = () => {
+    // connect to firebase
+    // db.collection(this.props.matchId)
+
+    // check if everyone was tagged
+      // tagger = winner
+
+  }
   render() {
     return (
-      <div>
-        GAME FINISHED
-        <Link to={routes.PROFILE}>
-          go home
-        </Link>
-      </div>
+      <Box>
+        
+      </Box>
     )
   }
 }
