a1ade866b53f025a81af1174b803f132617e3cd3
tamari-gray <tamari-gray>
2019-10-13 16:38:02 +1300

    home button on finished game

--- src/components/game/FinishedGame.jsx
+++ src/components/game/FinishedGame.jsx
@@ -1,38 +1,37 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
-import routes from '../../routes'
-import { Box } from 'grommet'
-import { db, geo } from '../../firebase'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import routes from "../../routes";
+import { Box } from "grommet";
+import { db, geo } from "../../firebase";
+import { Button } from "grommet";
 
 export class FinishedGame extends Component {
   state = {
     winners: []
-  }
+  };
 
   getWinners = () => {
     // connect to firebase
     // db.collection(this.props.matchId)
-
     // check if everyone was tagged
-      // tagger = winner
-
-  }
+    // tagger = winner
+  };
   render() {
     return (
       <Box>
-        yeet
+        finished! <br />
+        <Button as={Link} to="/profile" primary label="go home" />
       </Box>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => ({
-  
-})
+const mapStateToProps = state => ({});
 
-const mapDispatchToProps = {
-  
-}
+const mapDispatchToProps = {};
 
-export default connect(mapStateToProps, mapDispatchToProps)(FinishedGame)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(FinishedGame);
