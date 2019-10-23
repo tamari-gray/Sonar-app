65693ec2fdc0d8700c63c4f096e3f6674a182c25
tamari-gray <tamari-gray>
2019-10-23 20:54:29 +1300

    wrong firebase config lol

--- src/firebase.js
+++ src/firebase.js
@@ -14,13 +14,13 @@ import { GeoFirestore } from "geofirestore";
 // };
 
 const tamarimon97FirebaseConfig = {
-  apiKey: "AIzaSyCUu8zzfotKZNvfQcXRQm2LMX7xwMRCyRg",
-  authDomain: "sonar-2664d.firebaseapp.com",
-  databaseURL: "https://sonar-2664d.firebaseio.com",
-  projectId: "sonar-2664d",
-  storageBucket: "sonar-2664d.appspot.com",
-  messagingSenderId: "85025589952",
-  appId: "1:85025589952:web:772471903454f716"
+  apiKey: "AIzaSyBbTgM4qYDd9FxzKLZATux-i4Cz1tMoUb8",
+  authDomain: "sonar-a3366.firebaseapp.com",
+  databaseURL: "https://sonar-a3366.firebaseio.com",
+  projectId: "sonar-a3366",
+  storageBucket: "sonar-a3366.appspot.com",
+  messagingSenderId: "591806355157",
+  appId: "1:591806355157:web:34c9c60179e68488"
 };
 firebase.initializeApp(tamarimon97FirebaseConfig);
 
