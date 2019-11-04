d94c067a005d1d2329c18933f908ce2148576792
tamari <tamarigray97@gmail.com>
2019-11-04 23:41:01 +1300

    bailed on dotenv for now

--- src/firebase.js
+++ src/firebase.js
@@ -3,17 +3,27 @@ import "firebase/auth";
 import "firebase/firestore";
 import { GeoFirestore } from "geofirestore";
 
-const config = {
-  apiKey: process.env.REACT_APP_FB_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DB_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+// const config = {
+//   apiKey: process.env.REACT_APP_FB_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   databaseURL: process.env.REACT_APP_DB_URL,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID
+// }
+
+const REACT_APP_TAMARI_GRAY_FB_CONFIG = {
+  apiKey: "AIzaSyCUu8zzfotKZNvfQcXRQm2LMX7xwMRCyRg",
+  authDomain: "sonar-2664d.firebaseapp.com",
+  databaseURL: "https://sonar-2664d.firebaseio.com",
+  projectId: "sonar-2664d",
+  storageBucket: "sonar-2664d.appspot.com",
+  messagingSenderId: "85025589952",
+  appId: "1:85025589952:web:772471903454f716"
 }
 
-firebase.initializeApp(config);
+firebase.initializeApp(REACT_APP_TAMARI_GRAY_FB_CONFIG);
 
 export const auth = firebase.auth();
 export const db = firebase.firestore();
